f2c32c6c5a74ce6e665751e7de971a45f64d09ec
Mallory Goose <honk@local-goose>
2023-07-17 21:11:52 +0000

    aaaand linux uses globl instead of global that is unnecessary

--- practice.s
+++ practice.s
@@ -4,13 +4,13 @@ msg: .asciz "hello world\n"
 len = . - msg
 
 .text
-.global _start
 
+.globl _start
 _start:
 	ldr	w1, =msg	/*as this is RISC, we load from mem, not just mov*/
 	ldr	w2, =len
 	mov	w0, #1		/* standard output*/
 	mov	w7, #4		/* ID for write */
-	swi	#0		/* syscall happens here, calls write*/
+	svc	#0		/* syscall happens here, calls write*/
 	mov	w7, #1		/* ID for exit*/
-	swi	#0	/* call exit*/
+	svc	#0	/* call exit*/
