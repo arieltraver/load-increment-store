31c03f8ae34e01cbc8d0be6290a3f00e2712bdfe
Mallory Goose <honk@local-goose>
2023-07-17 20:50:18 +0000

    WHY DOES LINUX ARM ASSEMBLY USE W/X instead of R

--- practice.s
+++ practice.s
@@ -1,16 +1,16 @@
-@practice ref: https://helloworld.raspberrypi.org/articles/hw16-assembly-language-on-the-pi
+/*practice ref: https://helloworld.raspberrypi.org/articles/hw16-assembly-language-on-the-pi*/
 .data
 msg: .asciz "hello world\n"
-len: . - msg
+len = . - msg
 
 .text
 .global _start
 
 _start:
-	ldr	r1, =msg	@as this is RISC, we load from mem, not just mov 
-	ldr	r2, =len
-	mov	r0, #1		@ standard output
-	mov	r7, #4		@ ID for write
-	swi	#0		@ syscall happens here, calls write
-	mov	r7, #1		@ ID for exit
-	swi	#0		@ call exit
+	ldr	w1, =msg	/*as this is RISC, we load from mem, not just mov*/
+	ldr	w2, =len
+	mov	w0, #1		/* standard output*/
+	mov	w7, #4		/* ID for write */
+	swi	#0		/* syscall happens here, calls write*/
+	mov	w7, #1		/* ID for exit*/
+	swi	#0	/* call exit*/
